0c0c200bfcf48310928927640badce8114e8bd14
Ivan Batuhtin <vania.batuhtin@gmail.com>
2023-05-14 19:58:03 +0500

    App.js without JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,36 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { createElement } from "react";
 
 export const App = () => {
-	// Императивный стиль
 	const currentDate = new Date();
 	const currentYear = currentDate.getFullYear();
-	// Декларативный стиль
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{currentYear}</p>
-			</header>
-		</div>
+
+	return createElement(
+		"div",
+		{ className: "App" },
+		createElement(
+			"header",
+			{ className: "App-header" },
+			createElement("img", {
+				src: logo,
+				className: "App-logo",
+				alt: "logo",
+			}),
+			createElement("p", {
+				children: "Edit src/App.js and save to reload",
+			}),
+			createElement(
+				"a",
+				{
+					className: "App-link",
+					href: "https://reactjs.org",
+					target: "_blank",
+					rel: "noopener noreferrer",
+				},
+				"Learn React"
+			),
+			createElement("p", { children: currentYear })
+		)
 	);
 };
